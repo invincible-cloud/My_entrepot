275b6b46ddb2a97f34531c562ac6887d58dbed42
invincible-cloud <Dingyuxin12@qq.com>
2024-08-28 20:48:03 +0800

    测试

---
+++ act.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: act.c
+	> Author: dingxin 
+	> Mail: dingyuxin12@qq.com
+	> Created Time: Wed 28 Aug 2024 08:45:25 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+nt main(){
+    printf("hello world");
+    return 0;
+}
